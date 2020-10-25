2644dd04dd989ef34f10e675798cf2d6269f46bd
Julia Ballo <ballo.julia@gmail.com>
2020-10-25 16:59:48 -0700

    First Commit

---
+++ jsArrays.js
@@ -0,0 +1,32 @@
+//exercise 25
+// Define lottoNumbers below:
+const lottoNumbers = [99, 2, 13, 54, 30, 7];
+
+//exercise 26
+const leaderboard = ['Harry', 'Lua', 'Hermoine', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
+
+//exercise 27
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets[0] = 'Mercury';
+planets.push('Saturn');
+
+//exercise 28
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+airplaneSeats[3][1]= 'Hugo';
+
+//exercise 29
